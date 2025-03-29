9762c7cdd061360eb6301b571e238c8e2a69d501
STC <stc1988@users.noreply.github.com>
2025-03-29 18:37:48 +0900

    delete comment

--- hello_world/app.py
+++ hello_world/app.py
@@ -14,21 +14,13 @@ metrics = Metrics(namespace="Powertools")
 @app.get("/hello")
 @tracer.capture_method
 def hello():
-    # adding custom metrics
-    # See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/metrics/
     metrics.add_metric(name="HelloWorldInvocations", unit=MetricUnit.Count, value=1)
 
-    # structured log
-    # See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/logger/
     logger.info("Hello world API - HTTP 200")
     return {"message": "hello world"}
 
-# Enrich logging with contextual information from Lambda
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
-# Adding tracer
-# See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/tracer/
 @tracer.capture_lambda_handler
-# ensures metrics are flushed upon request completion/failure and capturing ColdStart metric
 @metrics.log_metrics(capture_cold_start_metric=True)
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
     return app.resolve(event, context)
